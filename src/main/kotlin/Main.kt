2bc1d621e1777aa45aa549b2c0b26edcc5fcfa96
syauqikhosyi <2010512141@mahasiswa.upnvj.ac.id>
2022-11-30 20:38:04 +0700

    The first version of CarShowroom

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var syauqiKhosyi = Account(
+        "syauqikhosyi",
+        "Syauqi",
+        "syauqikhosyi@gmail.com",
+        "+62855883399378",
+        true
+    )
+
+
+}
---
+++ src/main/kotlin/carData/UsedCar.kt
@@ -0,0 +1,13 @@
+package carData
+
+class UsedCar (
+    inputName : String,
+    inputBrand : String,
+    inputModel: String,
+    inputColor: String,
+    inputYear: Int,
+    inputKM : Double
+) :
+    Car(inputName, inputBrand, inputModel, inputColor, inputYear, inputKM) {
+
+}
